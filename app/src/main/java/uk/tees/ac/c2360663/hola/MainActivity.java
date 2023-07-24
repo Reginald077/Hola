0131e6345df354f227b75be54f98bc7d519cfb95
Reginald077 <124705078+Reginald077@users.noreply.github.com>
2023-07-24 16:14:19 +0100

    Initial commit

---
+++ app/src/main/java/uk/tees/ac/c2360663/hola/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.tees.ac.c2360663.hola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
